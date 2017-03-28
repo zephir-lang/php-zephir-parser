102681f629bbe4e9dc124b2a4ff57dacd5581857
Serghei Iakovlev <serghei@phalconphp.com>
2017-03-28 08:39:52 +0300

    Improved Appveyor build

--- install-win32-php5.bat
+++ install-win32-php5.bat
@@ -1,6 +1,8 @@
+cd parser
+
 cl lemon.c
 del parser.c parser.h scanner.c
 re2c -o scanner.c scanner.re
-lemon -s parser.php7.lemon
-type parser.php7.c > parser.c
+lemon -s parser.php5.lemon
+type parser.php5.c > parser.c
 type base.c >> parser.c
