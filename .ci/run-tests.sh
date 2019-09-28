70680d9ec445f1b5d051cab828ca9dbe6f40f7ab
Serghei Iakovlev <sadhooklay@gmail.com>
2019-09-28 16:29:35 +0300

    Improved run-tests.sh script [skip appveyor]

--- .ci/run-tests.sh
+++ .ci/run-tests.sh
@@ -7,21 +7,26 @@
 # For the full copyright and license information, please view
 # the LICENSE file that was distributed with this source code.
 
-export NO_INTERACTION=1
-export REPORT_EXIT_STATUS=1
-export ZEND_DONT_UNLOAD_MODULES=1
-export USE_ZEND_ALLOC=0
+set -e
 
+NO_INTERACTION=1
+REPORT_EXIT_STATUS=1
+ZEND_DONT_UNLOAD_MODULES=1
+USE_ZEND_ALLOC=0
+
+export NO_INTERACTION REPORT_EXIT_STATUS ZEND_DONT_UNLOAD_MODULES USE_ZEND_ALLOC
 if [ -z "${TEST_PHP_EXECUTABLE}" ]; then
-	export TEST_PHP_EXECUTABLE=$(phpenv which php)
+	TEST_PHP_EXECUTABLE="$(phpenv which php)"
+  export TEST_PHP_EXECUTABLE
 fi
 
-PHP_VERNUM="$(`phpenv which php-config` --vernum)"
-PROJECT_ROOT=$(readlink -enq "$(dirname $0)/../")
+PHP_VERNUM="$($(phpenv which php-config) --vernum)"
+PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
 
 if [ "${PHP_VERNUM}" -lt 70300 ]; then
-	if [ $(command -v valgrind 2>/dev/null) != "" ]; then
-		export TEST_PHP_ARGS=-m
+	if [ -n "$(command -v valgrind 2>/dev/null)" ]; then
+		TEST_PHP_ARGS=-m
+    export TEST_PHP_ARGS
 	else
 		>&2 echo "Skip check for memory leaks. Valgring does not exist"
 	fi
@@ -29,11 +34,11 @@ else
 	>&2 echo "Skip check for memory leaks due to unstable PHP version"
 fi
 
-${TEST_PHP_EXECUTABLE} ${PROJECT_ROOT}/run-tests.php \
+${TEST_PHP_EXECUTABLE} "${PROJECT_ROOT}/run-tests.php" \
 	-d extension=zephir_parser.so \
-	-d extension_dir=${PROJECT_ROOT}/modules \
+	-d extension_dir="${PROJECT_ROOT}/modules" \
 	-d variables_order=EGPCS \
-	-n ${PROJECT_ROOT}/tests/*.phpt \
+	-n "${PROJECT_ROOT}/tests/*.phpt" \
 	-g "FAIL,XFAIL,BORK,WARN,SKIP" \
 	--offline \
 	--show-diff \
