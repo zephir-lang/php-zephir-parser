484c85e6623555346e630032856a9a57ffe7f9f4
Serghei Iakovlev <sadhooklay@gmail.com>
2019-09-29 02:19:12 +0300

    Improved after_failure script [skip appveyor]

--- .ci/after-failure.sh
+++ .ci/after-failure.sh
@@ -7,10 +7,6 @@
 # For the full copyright and license information, please view
 # the LICENSE file that was distributed with this source code.
 
-# -e  Exit immediately if a command exits with a non-zero status.
-# -u  Treat unset variables as an error when substituting.
-set -eu
-
 shopt -s nullglob
 
 export LC_ALL=C
@@ -32,7 +28,7 @@ function install_gdb() {
 
 for i in /tmp/core.php.*; do
   install_gdb
-	(>&1 printf "Found core dump file: %s\\n\\n" "$i")
+  (>&1 printf "Found core dump file: %s\\n\\n" "$i")
   gdb -q "$(phpenv which php)" "$i" <<EOF
 set pagination 0
 backtrace full
