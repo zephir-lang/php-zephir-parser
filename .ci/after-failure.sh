c9f77b940425ddf6d8d453751db30d6cafb80c6b
Serghei Iakovlev <sadhooklay@gmail.com>
2019-12-08 18:21:04 +0200

    Remove no longer needed Travis CI support

--- .ci/after-failure.sh
+++ .ci/after-failure.sh
@@ -17,17 +17,7 @@ do
   (>&1 printf ">>> START (%d)\\n%s\\n<<< END (%d)\\n\\n" $count "$(cat "$file")" $count)
 done < <(find ./tests -type f \( -name '*.out' -o -name '*.mem' \) -print0)
 
-# for some reason Ubuntu 18.04 on Travis CI doesn't install gdb
-function install_gdb() {
-  if [ "${CI}" = "true" ] && [ "$(command -v gdb 2>/dev/null)" = "" ]
-  then
-    (>&1 echo "Install gdb...")
-    sudo apt-get install --no-install-recommends --quiet --assume-yes gdb 1> /dev/null
-  fi
-}
-
 for i in /tmp/core.php.*; do
-  install_gdb
   (>&1 printf "Found core dump file: %s\\n\\n" "$i")
   gdb -q "$(phpenv which php)" "$i" <<EOF
 set pagination 0
