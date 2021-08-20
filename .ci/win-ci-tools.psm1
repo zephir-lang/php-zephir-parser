d1ad6c0386414d0585264b3917b58e53642d0787
Anton Vasiliev <aionbase@gmail.com>
2021-08-20 22:22:06 +0100

    #110 - Add `Expand-Item7zip` function

--- .ci/win-ci-tools.psm1
+++ .ci/win-ci-tools.psm1
@@ -118,3 +118,30 @@ function DownloadFile {
         }
     }
 }
+
+function Expand-Item7zip {
+    <#
+        .SYNOPSIS
+            Extracts ZIP archives to specified directory
+    #>
+
+    param(
+        [Parameter(Mandatory = $true)] [System.String] $Archive,
+        [Parameter(Mandatory = $true)] [System.String] $Destination
+    )
+
+    if (-not (Test-Path -Path $Archive -PathType Leaf)) {
+        throw "Specified archive File is invalid: [$Archive]"
+    }
+
+    if (-not (Test-Path -Path $Destination -PathType Container)) {
+        New-Item $Destination -ItemType Directory | Out-Null
+    }
+
+    $Result = (& 7z x "$Archive" "-o$Destination" -aoa -bd -y -r)
+
+    if ($LastExitCode -ne 0) {
+        Write-Output "An error occurred while unzipping [$Archive] to [$Destination]. Error code was: ${LastExitCode}"
+        Exit $LastExitCode
+    }
+}
