f328620031bf2cce300585b30b783e16661e2e78
Anton Vasiliev <aionbase@gmail.com>
2021-08-20 22:13:54 +0100

    #110 - Add `InstallPhpSdk` function

--- .ci/win-ci-tools.psm1
+++ .ci/win-ci-tools.psm1
@@ -23,7 +23,7 @@ function SetupCommonEnvironment {
     git config --global advice.detachedHead false
 }
 
-Function InitializeReleaseVars {
+function InitializeReleaseVars {
 	If ($Env:BUILD_TYPE -Match "nts") {
 		$Env:RELEASE_ZIPBALL = "zephir_parser_${Env:PHP_ARCH}_vc${Env:VC_VERSION}_php${Env:PHP_VERSION}-nts_${Env:BUILD_VERSION}"
 
@@ -43,4 +43,32 @@ Function InitializeReleaseVars {
 	}
 
 	$Env:RELEASE_PATH = "${Env:APPVEYOR_BUILD_FOLDER}\${Env:RELEASE_FOLDER}"
+}
+
+function InstallPhpSdk {
+    <#
+        .SYNOPSIS
+            Install PHP SDK binary tools from sources.
+    #>
+
+    Write-Output "Install PHP SDK binary tools: ${env:PHP_SDK_VERSION}"
+
+    $PhpSdk = "php-sdk-${env:PHP_SDK_VERSION}.zip"
+    $RemoteUrl = "https://github.com/microsoft/php-sdk-binary-tools/archive/${PhpSdk}"
+    $DestinationPath = "C:\Downloads\${PhpSdk}"
+
+    if (-not (Test-Path $env:PHP_SDK_PATH)) {
+        if (-not [System.IO.File]::Exists($DestinationPath)) {
+            Write-Output "Downloading PHP SDK binary tools: $RemoteUrl ..."
+            DownloadFile $RemoteUrl $DestinationPath
+        }
+
+        $DestinationUnzipPath = "${env:Temp}\php-sdk-binary-tools-php-sdk-${env:PHP_SDK_VERSION}"
+
+        if (-not (Test-Path "$DestinationUnzipPath")) {
+            Expand-Item7zip $DestinationPath $env:Temp
+        }
+
+        Move-Item -Path $DestinationUnzipPath -Destination $env:PHP_SDK_PATH
+    }
 }
