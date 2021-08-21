f22648b3ed656a8d0f90a07fb72b1427af613d4b
Anton Vasiliev <aionbase@gmail.com>
2021-08-21 11:23:53 +0100

    #110 - Add `AppendSessionPath` function

--- .ci/AppVeyor.psm1
+++ .ci/AppVeyor.psm1
@@ -13,41 +13,4 @@ Function InitializeBuildVars {
 	}
 }
 
-Function AppendSessionPath {
-	[string[]] $PathsCollection = @(
-		"${Env:VSCOMNTOOLS}\..\..\VC",
-		"C:\Program Files (x86)\Microsoft Visual Studio ${Env:VC_VERSION}.0\VC",
-		"C:\Program Files (x86)\Microsoft Visual Studio ${Env:VC_VERSION}.0\VC\bin",
-		"${Env:VSCOMNTOOLS}",
-		"C:\php"
-		"C:\php\bin"
-		"C:\php-sdk\bin",
-		"C:\php-devpack"
-	)
 
-	$CurrentPath = (Get-Item -Path ".\" -Verbose).FullName
-
-	ForEach ($PathItem In $PathsCollection) {
-		Set-Location Env:
-		$AllPaths = (Get-ChildItem Path).value.split(";")  | Sort-Object -Unique
-		$AddToPath = $true
-
-		ForEach ($AddedPath In $AllPaths) {
-			If (-not "${AddedPath}") {
-				continue
-			}
-
-			$AddedPath = $AddedPath -replace '\\$', ''
-
-			If ($PathItem -eq $AddedPath) {
-				$AddToPath = $false
-			}
-		}
-
-		If ($AddToPath) {
-			$Env:Path += ";$PathItem"
-		}
-	}
-
-	Set-Location "${CurrentPath}"
-}
