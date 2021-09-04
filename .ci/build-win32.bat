1d2d4d25c0154eb2c09bd3c5e68b7121907d352c
AlexNDRmac <alexndr.mac@gmail.com>
2021-09-04 20:51:40 +0300

    Fix echo comand for parser first string

--- .ci/build-win32.bat
+++ .ci/build-win32.bat
@@ -14,6 +14,6 @@ cl lemon.c
 DEL zephir.c zephir.h parser.c scanner.c
 re2c -o scanner.c scanner.re
 lemon -s zephir.lemon
-ECHO #include ^<php.h^> > parser.c
+ECHO "#include ^<php.h^>" > parser.c
 TYPE zephir.c >> parser.c
 TYPE base.c >> parser.c
