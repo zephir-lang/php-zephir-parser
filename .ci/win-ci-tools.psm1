f2ee03df1e0d0eebf03b0be9ebac82eb31a49e04
AlexNDRmac <alexndr.mac@gmail.com>
2021-09-05 15:45:37 +0300

    Fix release path and artifact name

--- .ci/win-ci-tools.psm1
+++ .ci/win-ci-tools.psm1
@@ -29,25 +29,26 @@ function InitializeReleaseVars {
             Configures Environment variables for Release build.
     #>
 
-    if ($env:BUILD_TYPE -Match "nts") {
-        $env:RELEASE_ZIPBALL = "zephir_parser_${env:PHP_ARCH}_vc${env:VC_VERSION}_php${env:PHP_MINOR}_nts"
-
-        if ($env:PHP_ARCH -eq 'x86') {
-            $env:RELEASE_FOLDER = "x64\Release"
-        } else {
-            $env:RELEASE_FOLDER = "x64\Release"
-        }
-    } else {
-        $env:RELEASE_ZIPBALL = "zephir_parser_${env:PHP_ARCH}_vc${env:VC_VERSION}_php${env:PHP_MINOR}"
-
-        if ($env:PHP_ARCH -eq 'x86') {
-            $env:RELEASE_FOLDER = "x64\Release_TS"
-        } else {
-            $env:RELEASE_FOLDER = "x64\Release_TS"
-        }
+    # All extension builds should respect next logic for build artifacts
+    #
+    # release
+    # |---- x64
+    # |     |---- ts  / php_zephir_parser.dll
+    # |     |---- nts / php_zephir_parser.dll
+    # |---- x86
+    # |     |---- ts  / php_zephir_parser.dll
+    # |     |---- nts / php_zephir_parser.dll
+    #
+    # Build artifacts should be names like this:
+    # zephir-parser-php-7.0-nts-Win32-VC14-x86.zip
+    # zephir-parser-php-7.0-ts-Win32-VC14-x64.zip
+    $VC_Prefix = "VC"
+    if (${env:VC_VERSION} -ge 16) {
+        $VC_Prefix = "VS"
     }
 
-    $env:RELEASE_DLL_PATH = "${env:GITHUB_WORKSPACE}\${env:RELEASE_FOLDER}\${env:EXTENSION_FILE}"
+    $env:RELEASE_DLL_PATH = "${env:GITHUB_WORKSPACE}\${env:RELEASE_FOLDER}\${ArchPrefix}\${ThreadSafetyPrefix}\${env:EXTENSION_FILE}"
+    $env:RELEASE_ZIPBALL = "zephir-parser-php-${env:PHP_MINOR}-${env:BUILD_TYPE}-Win32-${VC_Prefix}${env:VC_VERSION}-${env:PHP_ARCH}"
 
     Write-Output "RELEASE_ZIPBALL=${env:RELEASE_ZIPBALL}" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
     Write-Output "RELEASE_DLL_PATH=${env:RELEASE_DLL_PATH}" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
